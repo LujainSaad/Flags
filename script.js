9e3884eac1c57463c0baf61115d3197e5ca351c9
Lujain <79986157+LujainSaad@users.noreply.github.com>
2022-03-05 22:13:55 +0300

    second commit

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 //  https://restcountries.com/v2/
 
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
@@ -35,3 +55,76 @@ const getCountryData = function (country) {
 
 getCountryData('saudi');
 getCountryData('uae');
+*/
+
+///////////////////////////////////////
+// Welcome to Callback Hell
+/*
+const getCountryAndNeighbor = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbor country (2)
+    const [neighbor] = data.borders; //first border
+
+    if (!neighbor) return; //if country has no border
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbor}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbor');
+    });
+  });
+};
+
+getCountryAndNeighbor('saudi');
+// getCountryAndNeighbor('uae');
+*/
+
+///////////////////////////////////////
+// Consuming Promises
+
+/*
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+}; // new way (consuming promises)
+
+getCountryData('saudi');
+*/
+
+///////////////////////////////////////
+// Chaining Promises
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbor = data[0].borders[0];
+
+      if (!neighbor) return;
+
+      return fetch(`https://restcountries.com/v2/alpha/${neighbor}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbor'));
+};
+
+getCountryData('saudi');
